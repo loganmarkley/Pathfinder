4b7d94a5d086e759df649c9ab4e06b50d27e99b4
Logan Markley <lcmkbc@mst.edu>
2024-10-21 18:33:23 -0500

    landing page draft done

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, MatCardModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.less'
 })
