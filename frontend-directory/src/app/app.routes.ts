7c598d24e1fcac5e2de53c556b4edfcfa3ed0817
Shreyas Mocherla <vmgng@umsystem.edu>
2024-11-16 10:55:18 -0600

    Added about us page, redirects on button click

--- frontend-directory/src/app/app.routes.ts
+++ frontend-directory/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { RouteGeneratorComponent } from './route-generator/route-generator.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'generator', component: RouteGeneratorComponent },
+  { path: 'about-us', component: AboutUsComponent}
 ];
