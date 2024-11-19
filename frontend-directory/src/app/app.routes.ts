c2954607180ad565397650370b7eca714a46969c
Logan Markley <lcmkbc@mst.edu>
2024-11-19 16:26:38 -0600

    updateForBackendTesting

--- frontend-directory/src/app/app.routes.ts
+++ frontend-directory/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { RouteGeneratorComponent } from './route-generator/route-generator.component';
+import { GenerationPageComponent } from './generation-page/generation-page.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
-  { path: 'generator', component: RouteGeneratorComponent },
+  { path: 'generator', component: GenerationPageComponent },
 ];
