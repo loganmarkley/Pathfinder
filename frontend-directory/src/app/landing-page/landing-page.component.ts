c17d085893e85cb4c13d4ed1f9e39a25c2036928
Logan Markley <lcmkbc@mst.edu>
2024-12-02 23:48:45 -0600

    try it out button now sends the user to the generate page

--- frontend-directory/src/app/landing-page/landing-page.component.ts
+++ frontend-directory/src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavBarComponent, MatCardModule],
+  imports: [NavBarComponent, MatCardModule, RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.less'
 })
